483f939ad0268d2f638829bfa85d8e072722a8ed
simons1008 <einmorgen@tdmail.de>
2026-07-28 17:45:59 +0200

    Programm an die Konstruktionsanleitung angepasst

--- SW/10_Aufgaben_mit_Funktionen_loesen/saetze_bauen.py
+++ SW/10_Aufgaben_mit_Funktionen_loesen/saetze_bauen.py
@@ -1,29 +1,35 @@
-# Das Programm soll Subjekt, Prädikat, Objekt aus Listen 
-# zufällig auswählen und einen Satz bauen
+# Das Programm soll zufällige Sätze ausgeben
 
 # Bibliothek importieren
 import random
 
-# Beispielsätze
+# Listen
 subjekt = ["Der Hund", "Die Journalistin", "Der Maler"] 
 prädikat = ["vergräbt", "interviewt", "malt"]	
 objekt = ["den Knochen", "den Bürgermeister", "ein Bild"]
 
-# Input der Funktion sind die Listen Subjekt, Prädikat und Objekt 
+# Kurzbeschreibung
+# Die Funktion soll Subjekt, Prädikat, Objekt aus Listen zufällig auswählen
+# und einen Satz bauen
+
+# Datenanalyse
+# Input der Funktion sind die (globalen) Listen Subjekt, Prädikat und Objekt 
 # Output der Funktion ist der Satz
 
-# Funktion mit Datentyp
-def bau_den_satz(subjekt: list[str], prädikat: list[str], \
-                 objekt: list[str]) -> str:
+# Funktion definieren
+def bau_den_satz() -> str:
+    # Funktionsrumpf
+    # zufälliges Element der Liste wählen
     mein_subjekt = random.choice(subjekt)
     mein_prädikat = random.choice(prädikat)
     mein_objekt = random.choice(objekt)
+    # Satz bauen
     mein_satz = mein_subjekt + " " + mein_prädikat + " " + mein_objekt
     return mein_satz	
 
-# Funktion aufrufen
+# Ergebnisse prüfen
 for i in range(5):
     # Satz bauen
-    mein_satz = bau_den_satz(subjekt, prädikat, objekt)
+    mein_satz = bau_den_satz()
     # Ergebnis drucken
     print(mein_satz)
