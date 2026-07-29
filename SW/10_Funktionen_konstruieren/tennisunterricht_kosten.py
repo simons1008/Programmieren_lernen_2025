5feea1be36d55a9e70b0fb8f6738f8ea74872e48
simons1008 <einmorgen@tdmail.de>
2026-07-29 19:56:41 +0200

    Programm an die Konstruktionsanleitung angepasst

--- SW/10_Funktionen_konstruieren/tennisunterricht_kosten.py
+++ SW/10_Funktionen_konstruieren/tennisunterricht_kosten.py
@@ -1,44 +1,55 @@
 # Programm vergleicht die Kosten von zwei Angeboten für Tennisunterricht 
 
-# 1. Angebot: Input der Funktion ist eine Integerzahl in der Einheit Stunde
-#             Output der Funktion ist eine Dezimalzahl in der Einheit EUR
+# Kurzbeschreibung
+# Funktion berechnet die Kosten für das Angebot Tennis Pro
 
-# Funktion mit Datentyp 
-def tennis_pro(trainerstunden: int) -> float:
+# Datenanalyse
+# Input der Funktion ist eine Dezimalzahl in der Einheit Stunde
+# Output der Funktion ist eine Dezimalzahl in der Einheit EUR
+# Trainer: 30 EUR/Stunde   Platzmiete: 20 EUR/Stunde
+
+# Funktion definieren
+def tennis_pro(trainerstunden: float) -> float:
     kosten = (20 + 30) * trainerstunden
     return kosten
 
-# 2. Angebot: Input der Funktion ist eine Integerzahl in der Einheit Stunde
-#             Output der Funktion ist eine Dezimalzahl in der Einheit EUR
+# Kurzbeschreibung
+# Funktion berechnet die Kosten für das Angebot Go-Tennis 
 
-# Funktion mit Datentyp
-def go_tennis(trainerstunden: int) -> float:
+# Datenanalyse
+# Input der Funktion ist eine Dezimalzahl in der Einheit Stunde
+# Output der Funktion ist eine Dezimalzahl in der Einheit EUR
+# Trainer: 25 EUR/Stunde   Platzmiete: 260 EUR/Saison
+
+# Funktion definieren
+def go_tennis(trainerstunden: float) -> float:
     kosten = 260 + 25 * trainerstunden
     return kosten
 
+# Ergebnisse prüfen
 # Input Liste anlegen
 trainerstunden_liste = range(0, 13)
-# 1. Angebot: Output Liste (leer) anlegen
-kosten_liste1 = []
-# 2. Angebot: Output Liste (leer) anlegen
-kosten_liste2 = []
+# Output Liste (leer) von Tennis Pro anlegen
+tennis_pro_kosten_liste = []
+# Output Liste (leer) von Go-Tennis anlegen
+go_tennis_kosten_liste = []
 
 # Funktionen aufrufen
 for x in trainerstunden_liste:
-    kosten_liste1.append(tennis_pro(x))
-    kosten_liste2.append(go_tennis(x))
+    tennis_pro_kosten_liste.append(tennis_pro(x))
+    go_tennis_kosten_liste.append(go_tennis(x))
 
 # Ergebnisse drucken
 print("Trainerstunden  Tennis Pro  Go-Tennis")
-for x, y, z in zip(trainerstunden_liste, kosten_liste1, kosten_liste2):
+for x, y, z in zip(trainerstunden_liste, tennis_pro_kosten_liste, go_tennis_kosten_liste):
     print("{:14.2f}  {:10.2f}  {:9.2f}".format(x, y, z))
 
-# Modul für das Plotten von Graphen importieren
+# Bibliothek importieren
 import matplotlib.pyplot as plt
 
 # Ergebnisse plotten
-plt.plot(trainerstunden_liste, kosten_liste1)
-plt.plot(trainerstunden_liste, kosten_liste2)
+plt.plot(trainerstunden_liste, tennis_pro_kosten_liste)
+plt.plot(trainerstunden_liste, go_tennis_kosten_liste)
 plt.xlabel("Trainerstunden")
 plt.ylabel("Kosten")
 plt.show()
